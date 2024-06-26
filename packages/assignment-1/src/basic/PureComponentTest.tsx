3dbe911edea0cfd74c19e9e75b87f8bf8264499a
shiny1912 <shinyoungmo12@gmail.com>
2024-06-26 18:29:23 +0900

    1st week assignment

--- packages/assignment-1/src/basic/PureComponentTest.tsx
+++ packages/assignment-1/src/basic/PureComponentTest.tsx
@@ -2,21 +2,18 @@ import { memo, useMemo, useState } from "react";
 import { Cat, Dog } from "./PureComponentTest.components.tsx";
 import React from "react";
 
+const MemoCat = memo(Cat);
+const MemoDog = memo(Dog);
 
 // NOTE: 다른 파일은 수정하지 않고, 현재 파일만 수정하여 문제를 해결해주세요.
 export default function PureComponentTest() {
   const [meowCount, setMeowCount] = useState(1);
   const [barkedCount, setBarkedCount] = useState(1);
-  const Meow = memo(Cat);
-  const Bark = memo(Dog);
-
-  const memoizedMeowCount = useMemo(() => meowCount, [meowCount]);
-  const memoizedBarkCount = useMemo(() => barkedCount, [barkedCount]);
 
   return (
     <div>
-      <Meow crying={memoizedMeowCount} />
-      <Bark crying={memoizedBarkCount} />
+      <MemoCat crying={meowCount} />
+      <MemoDog crying={barkedCount} />
       <button data-testid="meow" onClick={() => setMeowCount(n => n + 1)}>야옹</button>
       <button data-testid="bark" onClick={() => setBarkedCount(n => n + 1)}>멍멍</button>
     </div>
