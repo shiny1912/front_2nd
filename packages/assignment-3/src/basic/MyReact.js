23480e86f1c59f7fa8c7a3967241ecdd1114d085
shiny1912 <shinyoungmo12@gmail.com>
2024-07-04 22:41:02 +0900

    basic assignment 3

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -2,8 +2,26 @@ import { createHooks } from "./hooks";
 import { render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  let oldNode = null;
+  let newNode = null;
+  let rootElement = null;
+  let rootComponent = null;
+
+  const _render = () => {
+    if (!rootElement || !rootComponent) return;
+    resetHookContext();
+    const newNode = rootComponent();
+    updateElement(rootElement, newNode, oldNode);
+    oldNode = newNode;
+  };
+
+  function render($root, component) {
+    oldNode = null;
+    rootElement = $root;
+    rootComponent = component;
+    //resetHookContext();
+    _render();
+  }
 
   const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
 
