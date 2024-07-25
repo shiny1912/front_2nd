554b21b259a84060b013218d7a89a06d51b0543f
shiny1912 <shinyoungmo12@gmail.com>
2024-07-25 21:26:35 +0900

    lighthouse 설정파일 추가

---
+++ packages/assignment-6/lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+        collect: {
+            staticDistDir: './packages/assignment-6/dist',
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
+    },
+};
